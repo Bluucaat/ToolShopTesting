c4b4acfa0ac8f62dada2304769cbb48de74de75b
Krisztian Ribar <krisztian_ribar@epam.com>
2025-07-17 12:17:17 +0200

    introduced eslint and prettier into the project

--- tests/step-definitions/login.steps.js
+++ tests/step-definitions/login.steps.js
@@ -1,31 +1,29 @@
-import { When, Then } from '@wdio/cucumber-framework';
+import { Before, When, Then } from '@wdio/cucumber-framework';
 import { expect } from '@wdio/globals';
 import LoginPage from '../../pages/login.page';
 
 When(/^I enter (a|an) (valid|invalid) "([^"]*)" and (.*)$/, async (_, __, email, password) => {
-    const actualPassword = password == 'validPassword'
-        ? String(process.env.VALID_LOGIN_PASSWORD)
-        : password
-    await LoginPage.enterCredentials(email, actualPassword);
+  const actualPassword =
+    password == 'validPassword' ? String(process.env.VALID_LOGIN_PASSWORD) : password;
+  await LoginPage.enterCredentials(email, actualPassword);
 });
 
 When('I click the login Button', async () => {
-    await LoginPage.loginButton.click();
+  await LoginPage.loginButton.click();
 });
 
-Then("I should see the {string} displayed", async (name) => {
-    const userNameElement = await $('[data-test="nav-menu"]');
-    await expect(userNameElement).toBeDisplayed();
+Then('I should see the {string} displayed', async (name) => {
+  const userNameElement = await $('[data-test="nav-menu"]');
+  await expect(userNameElement).toBeDisplayed();
 });
 
-Then("I should see an {string}", async (errorMessage) => {
-    let errorElement = LoginPage.errorMessageElement(errorMessage);
-    expect(await errorElement).toBeDisplayed();
+Then('I should see an {string}', async (errorMessage) => {
+  const errorElement = LoginPage.errorMessageElement(errorMessage);
+  expect(await errorElement).toBeDisplayed();
 });
 
-Then("I should remain on the login page", async () => {
-    const currentUrl = await browser.getUrl();
-    expect(currentUrl).toContain("/login");
-    await expect(LoginPage.loginForm).toBeDisplayed();
+Then('I should remain on the login page', async () => {
+  const currentUrl = await browser.getUrl();
+  expect(currentUrl).toContain('/login');
+  await expect(LoginPage.loginForm).toBeDisplayed();
 });
-
--- tests/step-definitions/navigation.steps.js
+++ tests/step-definitions/navigation.steps.js
@@ -1,18 +1,18 @@
 import { Given, Then } from '@wdio/cucumber-framework';
 
 Given('I am on the {string} page', async (pageName) => {
-    const urls = {
-        'home': 'https://practicesoftwaretesting.com',
-        'login': 'https://practicesoftwaretesting.com/auth/login'
-    };
-    await browser.url(urls[pageName]);
+  const urls = {
+    home: 'https://practicesoftwaretesting.com',
+    login: 'https://practicesoftwaretesting.com/auth/login',
+  };
+  await browser.url(urls[pageName]);
 });
 
 Then('the {string} page should be loaded', async (pageName) => {
-    const uniqueSelectors = {
-        'home': '//*[contains(text(), "Price Range")]',
-        'login': '[data-test = "login-form"]'
-    }
-    const uniqueElement = $(uniqueSelectors[pageName]);
-    await expect(uniqueElement).toBeDisplayed();
+  const uniqueSelectors = {
+    home: '//*[contains(text(), "Price Range")]',
+    login: '[data-test = "login-form"]',
+  };
+  const uniqueElement = $(uniqueSelectors[pageName]);
+  await expect(uniqueElement).toBeDisplayed();
 });
